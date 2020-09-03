6e298957688316b46761d7470e0a7939c6e35015
marina.kluch <marina.kluch@xpence.uk>
2020-09-03 17:45:30 +0600

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
+import { useMenu } from './menu';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import moment from 'moment';
+import 'moment/locale/ru';
+
+console.log(moment.locale());
+moment().locale('ru');
+console.log(moment.locale());
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const today = moment();
+	const tomorrow = moment().add(1, 'day');
+	return (
+		<div>
+			<h2>Сегодня {today.format('dddd')}</h2>
+			<Day dt={today} />
+			<h2>Завтра {tomorrow.format('dddd')}</h2>
+			<Day dt={tomorrow} />
+		</div>
+	);
 }
 
 export default App;
+
+const Day = ({ dt }) => {
+	const m = useMenu(dt);
+
+	return m && m.breakfast ? (
+		<div className='day'>
+			<div>
+				<div className='title'>Завтрак</div>
+				{m.breakfast.map((x, idx) => (
+					<div key={idx} className='name'>
+						{x}
+					</div>
+				))}
+			</div>
+			<div>
+				<div className='title'>Обед</div>
+				{m.lunch.map((x, idx) => (
+					<div className='name' key={idx}>
+						{x}
+					</div>
+				))}
+			</div>
+		</div>
+	) : (
+		'No data'
+	);
+};
