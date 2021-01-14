04aae38223649a9b5af613875e3ffdeef6cbe84e
marina.kluch <marina.kluch@xpence.uk>
2021-01-14 07:53:03 +0600

    prev next

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import { useMenu, useWorkDays } from './menu';
 import React from 'react';
 import moment from 'moment';
 import 'moment/locale/ru';
+import { useState } from 'react';
+import './App.css';
 
 console.log(moment.locale());
 moment().locale('ru');
 console.log(moment.locale());
 
 function App() {
-	const days = useWorkDays(moment(), 10);
+	const [diff, setDiff] = useState(0);
+	const days = useWorkDays(moment().add(diff, 'days'), 20);
 	return (
 		<div>
-			{days.map((d, idx) => (
-				<div key={idx}>
-					<h2> {d.format('dddd DD MMMM')}</h2>
-					<Day dt={d} />
+			<div className='tools'>
+				<div onClick={() => setDiff(diff - 7)} className='tool prev'>
+					❮Prev
 				</div>
-			))}
+				<div onClick={() => setDiff(diff + 7)} className='tool next'>
+					Next❯
+				</div>
+			</div>
+			<div>
+				{days.map((d, idx) => (
+					<div key={idx}>
+						<h2> {d.format('dddd DD MMMM')}</h2>
+						<Day dt={d} />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
